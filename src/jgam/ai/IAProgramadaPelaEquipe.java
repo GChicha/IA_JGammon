110df157f21b75d3c8493c3a3e7f78ff8219486d
GChicha <guilherme.chichanoski@gmail.com>
2017-05-28 15:47:25 -0300

    ia quase promta

--- src/jgam/ai/IAProgramadaPelaEquipe.java
+++ src/jgam/ai/IAProgramadaPelaEquipe.java
@@ -1,6 +1,8 @@
 package jgam.ai;
 
 import jgam.game.*;
+import java.util.List;
+import java.lang.Math;
 
 public class IAProgramadaPelaEquipe extends EvaluatingAI {
     /**
@@ -19,53 +21,51 @@ public class IAProgramadaPelaEquipe extends EvaluatingAI {
      * <p>A soma das pecas da ia em cada casa sera poderada considerando
      * o avanco</p>
      *
+     * <p>Até 270</p>
+     *
      * @param setup
      * @return float greater or equal than 0
      */
     private float heuristicaAvanco(BoardSetup setup) {
         int player = setup.getActivePlayer();
 
-        return setup.calcPip(player);
-    }
+        float pip = 0;
+        for (int i = 1; i <= 25; i++) {
+            pip += i * (-6.6914029106712682 + (3.2367315199142737 - 0.08274024712211484 * i)* i) * setup.getPoint(player, i);
+        }
 
+        return pip/8400;
+    }
+// Até 7
     private float heuristicaProtecao(BoardSetup setup) {
         int player = setup.getActivePlayer();
 
         float protegidos = 0;
 
-        for (int i = 0; i < 24; i++) {
+        for (int i = 1; i < 25; i++) {
             if (setup.getPoint(player, i) > 1) {
                 protegidos++;
             }
         }
-        return protegidos;
+
+        return protegidos/7;
     }
-
+// Até 15
     private float heuristicaAtaque(BoardSetup setup) {
-        float atacaveis = 0;
-
         int playerAtacante = setup.getActivePlayer();
         int playerDefensor = (playerAtacante == 1) ? 1: 2;
 
-        PossibleMoves possibleMoves = new PossibleMoves(setup);
-
-        java.util.List<java.util.List<SingleMove>> moveChains = possibleMoves.getPossibleMoveChains();
-        for (java.util.List  list : moveChains) {
-            for (Object move : list) {
-                int to = ((SingleMove)move).to();
-
-                atacaveis += setup.getPoint(playerDefensor, to);
-            }
-        }
-
-        return atacaveis;
+        return setup.getBar(playerDefensor)/15;
     }
 
     @Override
     public double propabilityToWin(BoardSetup setup) throws CannotDecideException {
         /* @todo só escreve aqui teu codigo */
 
-        return heuristicaProtecao(setup) + heuristicaAvanco(setup);
+        PossibleMoves possibleMoves = new PossibleMoves(setup);
+        setup.getDice();
+
+        return 0.4*heuristicaAtaque(setup) + 0.4*heuristicaProtecao(setup) + 0.2*heuristicaAvanco(setup);
     }
 
     /**
